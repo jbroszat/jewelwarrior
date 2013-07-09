0245ea8e3fffb6e10425abe73f9cf2d9190ff87e
Broszat <janis@ersatzfluessigkeit.de>
2013-07-10 01:32:28 +0200

    added board functions

--- scripts/board.js
+++ scripts/board.js
@@ -46,10 +46,68 @@ jewel.board = (function(){
 		return Math.floor(Math.random() * numJewelTypes)
 	}
 
+	function checkChain(x,y) {
+		var type = getJewel(x,y), left = 0, right = 0, down = 0, up = 0;
+
+		while (type === getJewel(x + right + 1, y)) {
+			right++;
+		}
+
+		while (type === getJewel(x - left - 1, y)) {
+			left++;
+		}
+
+		while (type === getJewel(x, y + up + 1)) {
+			up++;
+		}
+
+		while (type === getJewel(x, y - down - 1)) {
+			down++;
+		}
+
+		return Math.max(left + 1 + right, up + 1 + down);
+	}
+
+	function canSwap(x1,y1,x2,y2) {
+		var type1 = getJewel(x1,y1), type2 = getJewel(x2,y2), chain;
+
+		if (!isAdjacent(x1,y1,x2,y2)) {
+			return false;
+		}
+
+		jewels[x1][y1] = type2;
+		jewels[x2][y2] = type1;
+
+		chain = (checkChain(x2,y2) > 2 || checkChain(x1,y1) > 2);
+
+		jewels[x1][y1] = type1;
+		jewels[x2][y2] = type2;
+
+		return chain;
+	}
+
+	function isAdjacent (x1,y1,x2,y2) {
+		var dx = Math.abs(x1 - x2), dy = Math.abs(y1 - y2);
+		return (dx + dy === 1);
+	}
+
+	function getChains() {
+		var x,y,chains = [];
+
+		for (x = 0, x < cols; x++) {
+			chains[x] = [];
+			for (y = 0, y < rows; y++) {
+				chains[x][y] = checkChain(x,y);
+			}
+		}
+
+		return chains;
+	}
+
 	function print() {
 		var str = "";
-		for (var x = 0; x < rows; x++) {
-			for (var y = 0; y < cols; y++) {
+		for (var y = 0; y < rows; y++) {
+			for (var x = 0; x < cols; x++) {
 				str += getJewel(x,y) + " ";
 			}
 			str += "\n";
@@ -59,6 +117,8 @@ jewel.board = (function(){
 
 	return {
 		initialize:initialize,
+//		canSwap:canSwap,
+		getJewel:getJewel,
 		print:print
 	};
 })();
